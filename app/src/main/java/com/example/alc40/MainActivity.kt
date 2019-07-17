d0d7e1150cb03c40f0a69d02492d866c2116baa1
sibusiso93 <sibusiso.tom93@gmail.com>
2019-07-17 05:38:56 +0200

    update

--- app/src/main/java/com/example/alc40/MainActivity.kt
+++ app/src/main/java/com/example/alc40/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.alc40
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         button.setOnClickListener {  switch("About") }
         button2.setOnClickListener { switch("Profile") }
+        Glide
+            .with(this)
+            .load("https://firebasestorage.googleapis.com/v0/b/invitation-7c83a.appspot.com/o/Andela.jpeg?alt=media&token=fe39821f-6290-4a49-a8fd-60f0bf95a895")
+            .into(back)
     }
     fun switch(name:String)
     {
